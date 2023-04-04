f345d0a6424d94cc218102365c0b0c470e9fd377
Areri Rodney Areri <arerirodney@gmail.com>
2023-04-04 11:03:08 +0300

    login and authentication

--- index.js
+++ index.js
@@ -21,3 +21,60 @@ const navSlide = () => {
   };
   
   navSlide();
+
+ // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+  // Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyATYDhTzQUCuz46Pu3ledwICGe9J-I8u4w",
+  authDomain: "a-hospital-website.firebaseapp.com",
+  projectId: "a-hospital-website",
+  storageBucket: "a-hospital-website.appspot.com",
+  messagingSenderId: "17198961804",
+  appId: "1:17198961804:web:eb568fd979fce262d497a9",
+  measurementId: "G-FH90XBSB7S"
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the appointments collection in the database
+var appointmentsRef = firebase.database().ref('appointments');
+
+// Get the form element
+var appointmentForm = document.getElementById('appointment-form');
+
+// Add an event listener for when the form is submitted
+appointmentForm.addEventListener('submit', submitForm);
+
+// Function to submit the form data to the database
+function submitForm(event) {
+  // Prevent the form from submitting normally
+  event.preventDefault();
+
+  // Get the values from the form fields
+  var name = appointmentForm.querySelector('input[name="Name"]').value;
+  var number = appointmentForm.querySelector('input[name="number"]').value;
+  var email = appointmentForm.querySelector('input[name="email"]').value;
+  var date = appointmentForm.querySelector('input[name="date"]').value;
+
+  // Create a new appointment object with the form data
+  var newAppointment = {
+    name: name,
+    number: number,
+    email: email,
+    date: date
+  };
+
+  // Add the appointment data to the database
+  appointmentsRef.push(newAppointment);
+
+  // Reset the form fields
+  appointmentForm.reset();
+}
---
+++ signUp.js
@@ -0,0 +1,126 @@
+// require('dotenv').config();
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATYDhTzQUCuz46Pu3ledwICGe9J-I8u4w",
+  authDomain: "a-hospital-website.firebaseapp.com",
+  projectId: "a-hospital-website",
+  storageBucket: "a-hospital-website.appspot.com",
+  messagingSenderId: "17198961804",
+  appId: "1:17198961804:web:eb568fd979fce262d497a9",
+  measurementId: "G-FH90XBSB7S"
+};
+
+const app = initializeApp(firebaseConfig);
+// const database = getDatabase(app);
+const auth = getAuth();
+
+const submitButton = document.getElementById("submit");
+const signupButton = document.getElementById("sign-up");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const main = document.getElementById("main");
+const createacct = document.getElementById("create-acct");
+
+const signupEmailIn = document.getElementById("email-signup");
+const confirmSignupEmailIn = document.getElementById("confirm-email-signup");
+const signupPasswordIn = document.getElementById("password-signup");
+const confirmSignUpPasswordIn = document.getElementById(
+  "confirm-password-signup"
+);
+const createacctbtn = document.getElementById("create-acct-btn");
+
+const returnBtn = document.getElementById("return-btn");
+
+var email,
+  password,
+  signupEmail,
+  signupPassword,
+  confirmSignupEmail,
+  confirmSignUpPassword;
+
+createacctbtn.addEventListener("click", function () {
+  var isVerified = true;
+
+  signupEmail = signupEmailIn.value;
+  confirmSignupEmail = confirmSignupEmailIn.value;
+  if (signupEmail != confirmSignupEmail) {
+    window.alert("Email fields do not match. Try again.");
+    isVerified = false;
+  }
+
+  signupPassword = signupPasswordIn.value;
+  confirmSignUpPassword = confirmSignUpPasswordIn.value;
+  if (signupPassword != confirmSignUpPassword) {
+    window.alert("Password fields do not match. Try again.");
+    isVerified = false;
+  }
+
+  if (
+    signupEmail == null ||
+    confirmSignupEmail == null ||
+    signupPassword == null ||
+    confirmSignUpPassword == null
+  ) {
+    window.alert("Please fill out all required fields.");
+    isVerified = false;
+  }
+
+  if (isVerified) {
+    createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
+      .then((userCredential) => {
+        // Signed in
+        // const user = userCredential.user;
+        // ...
+        window.alert("Success! Account created.");
+        window.location = "./createTask.html";
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        window.alert("Error occurred. Try again.");
+        window.alert(errorMessage);
+      });
+  }
+});
+
+submitButton.addEventListener("click", function () {
+  email = emailInput.value;
+  // console.log(email);
+  password = passwordInput.value;
+  // console.log(password);
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+
+      window.alert("Success! Welcome back!");
+      window.location = "./createTask.html";
+
+      // ...
+    })
+    .catch((error) => {
+      // const errorCode = error.code;
+      // const errorMessage = error.message;
+      window.alert("Error occurred. Try again.");
+    });
+});
+
+signupButton.addEventListener("click", () => {
+  main.style.display = "none";
+  createacct.style.display = "block";
+});
+
+returnBtn.addEventListener("click", function () {
+  main.style.display = "block";
+  createacct.style.display = "none";
+});
