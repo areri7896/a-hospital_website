04a03c28de1657b45f67c8b1c61c1ffea395f80a
Areri Rodney Areri <arerirodney@gmail.com>
2023-04-09 22:39:46 +0300

    javascript code fix

--- index.js
+++ index.js
@@ -1,37 +1,25 @@
-const navSlide = () => {
-    const burger = document.querySelector(".Hamb");
-    const nav = document.querySelector(".navbar");
-    const navLinks = document.querySelectorAll(".navbar a");
-  
-    burger.addEventListener("click", () => {
-      nav.classList.toggle("nav-active");
-  
-      navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-          link.style.animation = "";
-        } else {
-          link.style.animation = `navLinkFade 0.5s ease forwards ${
-            index / 7 + 0.5
-          }s `;
-        }
-      });
-      burger.classList.toggle("toggle");
-    });
-    //
-  };
-  
-  navSlide();
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyATYDhTzQUCuz46Pu3ledwICGe9J-I8u4w",
   authDomain: "a-hospital-website.firebaseapp.com",
+  databaseURL: "https://a-hospital-website-default-rtdb.firebaseio.com",
   projectId: "a-hospital-website",
   storageBucket: "a-hospital-website.appspot.com",
   messagingSenderId: "17198961804",
   appId: "1:17198961804:web:eb568fd979fce262d497a9",
   measurementId: "G-FH90XBSB7S"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -70,3 +58,27 @@ function submitForm(event) {
   // Reset the form fields
   appointmentForm.reset();
 }
+
+const navSlide = () => {
+  const burger = document.querySelector(".Hamb");
+  const nav = document.querySelector(".navbar");
+  const navLinks = document.querySelectorAll(".navbar a");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s `;
+      }
+    });
+    burger.classList.toggle("toggle");
+  });
+  //
+};
+
+navSlide();
